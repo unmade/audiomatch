62686b033c938914098181bae35dc9c4faa8b456
Aleksei Maslakov <lesha.maslakov@gmail.com>
2020-04-01 22:47:06 +0300

    Allow to compare files and directories

--- src/audiomatch/cli.py
+++ src/audiomatch/cli.py
@@ -1,12 +1,13 @@
 import argparse
+import pathlib
 
 from audiomatch.match import match
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("path", type=str)
+    parser = argparse.ArgumentParser(prog="audiomatch")
+    parser.add_argument("path", type=pathlib.Path, nargs="+")
     parser.add_argument("--length", type=int, default=120)
     args = parser.parse_args()
 
-    match(args.path, args.length)
+    match(*args.path, length=args.length)
--- tests/test_fingerprints.py
+++ tests/test_fingerprints.py
@@ -5,20 +5,21 @@ import pytest
 from audiomatch import fingerprints
 
 
-def _load_fp(name: str) -> List[int]:
-    lines = resources.read_text("tests.data", name).splitlines()
+def _load_fp(filepath: str) -> List[int]:
+    subpackage, name = filepath.split("/")
+    lines = resources.read_text(f"tests.data.{subpackage}", name).splitlines()
     return [int(value) for value in lines[1].strip("FINGERPRINT=").split(",")]
 
 
 @pytest.mark.parametrize(
     ["a", "b", "score"],
     [
-        ("sample-1-take-1.log", "sample-1-take-1.log", 1.00),
-        ("sample-1-take-1.log", "sample-1-take-2.log", 0.77),
-        ("sample-1-take-1.log", "sample-2-take-2.log", 0.00),
-        ("sample-1-take-3.log", "sample-2-take-4.log", 0.00),
-        ("sample-2-take-1.log", "sample-2-take-2.log", 0.66),
-        ("sample-2-take-4.log", "sample-2-take-1.log", 0.63),
+        ("sample-1/take-1.log", "sample-1/take-1.log", 1.00),
+        ("sample-1/take-1.log", "sample-1/take-2.log", 0.77),
+        ("sample-1/take-1.log", "sample-2/take-2.log", 0.00),
+        ("sample-1/take-3.log", "sample-2/take-4.log", 0.00),
+        ("sample-2/take-1.log", "sample-2/take-2.log", 0.66),
+        ("sample-2/take-4.log", "sample-2/take-1.log", 0.63),
     ],
 )
 def test_compare(a, b, score):
