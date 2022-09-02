4b6e2150f69f8bb953f0d33417de769ab3701301
Aleksei Maslakov <lesha.maslakov@gmail.com>
2022-09-02 20:42:06 +0300

    Update linters

--- tests/test_cli_invocation.py
+++ tests/test_cli_invocation.py
@@ -3,6 +3,7 @@ import subprocess
 
 def test_invoke_from_command_line():
     output = subprocess.run(
-        ["python", "-m", "audiomatch", "--help"], stdout=subprocess.PIPE,
+        ["python", "-m", "audiomatch", "--help"],
+        stdout=subprocess.PIPE,
     )
     assert "usage: audiomatch" in output.stdout.decode()
